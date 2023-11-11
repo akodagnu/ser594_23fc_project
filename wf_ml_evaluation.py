c8efa366c19f59451d090545fa8486678648794e
akodagnu <akshata.bharadwaj@yahoo.com>
2023-11-11 13:13:48 -0700

    Computed metrics for model

--- wf_ml_evaluation.py
+++ wf_ml_evaluation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from wf_ml_training import create_model
+from wf_ml_prediction import predict
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
@@ -7,13 +8,21 @@ def create_train_test_set():
     df = pd.read_csv("data_processed/Occupancy_Estimation_processed.csv")
     df = shuffle(df)
     train_dataset, test_dataset = train_test_split(df, test_size=0.2)
-    train_dataset.to_csv("data_processed/train.csv")
-    test_dataset.to_csv("data_processed/test.csv")
+    train_dataset.to_csv("data_processed/train.csv", index=False)
+    test_dataset.to_csv("data_processed/test.csv", index=False)
     return train_dataset
 
 def train():
+    print("Creating train and test dataset")
     data = create_train_test_set()
+    print("Creating model")
     create_model(data)
+    print("Predicting output")
+    mse, r2_score = predict()
+    print("Writing metrics to summary file")
+    with open("evaluation/summary.txt","w+") as f:
+        f.write("Dataset \t\t\t\t\t\t\t Mean Squared Error \t\t\t R2 score \n")
+        f.write("Occupancy_Estimation_processed.csv" + "\t" + str(mse) + "\t\t\t" + str(r2_score))
     
 train()
 
---
+++ wf_ml_prediction.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import pickle
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+def predict():
+    with open('models/random_forest_model.pickle', 'rb') as file:
+        loaded_model = pickle.load(file)
+    test_data = pd.read_csv("data_processed/test.csv")
+    # Dropping the same columns as my train data
+    X_Test = test_data.drop(['Date', 'Time', 'Room_Occupancy_Count','S1_Temp','S2_Temp','S3_Temp','S4_Temp'], axis=1)
+    Y_Test = test_data['Room_Occupancy_Count']
+
+    Y_Pred = loaded_model.predict(X_Test)
+    mse = mean_squared_error(Y_Test, Y_Pred)
+    # print(f'Mean Squared Error: {mse}')
+
+    Y_Pred_Test = loaded_model.predict(X_Test)
+
+    # Calculate R-squared for test set
+    r2_score_predict = r2_score(Y_Test, Y_Pred_Test)
+    #print(f'R-squared (Test): {r2_test}')
+    return mse, r2_score_predict
